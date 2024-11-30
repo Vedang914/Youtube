2d930669ba527a3fb5e139fa56ece4dadc36682e
Vedang Nagar <vedang914@gmail.com>
2024-11-30 10:59:39 +0530

    Completed the HomePage

--- frontend/src/components/HomePage.jsx
+++ frontend/src/components/HomePage.jsx
@@ -1,10 +1,195 @@
-import React from 'react'
-import './HomePage.css'
+import React from "react";
+import "./HomePage.css";
 
-const HomePage = () => {
+const HomePage = ({ sideNavbar }) => {
+  const options = [
+    "All",
+    "Twenty20 Cricket",
+    "Music",
+    "Live",
+    "Mixes",
+    "Gaming",
+    "Debates",
+    "Coke Studio Pakistan",
+    "Democracy",
+    "Pakistani dramas",
+    "Comedy",
+    "Pakistani dramas",
+    "Comedy",
+  ];
   return (
-    <div>HomePage</div>
-  )
-}
+    <div className={sideNavbar ? "homePage" : "fullHomePage"}>
+      <div className="homePage_options">
+        {options.map((item, index) => {
+          return (
+            <div key={index} className="homePage_option">
+              {item}
+            </div>
+          );
+        })}
+      </div>
 
-export default HomePage
+      <div className={sideNavbar ? "home_mainPage" : "home_mainPageWithoutLink"}>
+
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+
+        <div className="youtube_Video">
+
+          <div className="youtube_thumbnailBox">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s"
+              className="youtube_thumbnailPic"
+              alt="Thumbnail"
+            />
+            <div className="youtube_timingThumbnail">28:05</div>
+          </div>
+
+          <div className="youtubeTitleBox">
+            <div className="youtubeTitleBoxProfile">
+              <img className="youtube_thumbnail_Profile" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTVo0keNjGR2EvD0iODP-dVmkoaqwFEtab-iw&s" />
+            </div>
+
+            <div className="youtubeTitleBox_Title">
+              <div className="youtube_videoTitle">User1</div>
+              <div className="youtube_channelName">User1</div>
+              <div className="youtubeVideo_views">3 likes</div>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
